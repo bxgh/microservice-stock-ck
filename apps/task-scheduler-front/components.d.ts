cbf9ae17b93fafa5b07d0dafa6d1be2906be7ac4
Task Scheduler Team <task-scheduler-team@example.com>
2025-11-15 11:39:32 +0800

    1

--- apps/task-scheduler-front/components.d.ts
+++ apps/task-scheduler-front/components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     BasicButton: typeof import('./src/components/basic/BasicButton.vue')['default']
     BasicCard: typeof import('./src/components/basic/BasicCard.vue')['default']
+    ElAlert: typeof import('element-plus/es')['ElAlert']
     ElAside: typeof import('element-plus/es')['ElAside']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
     ElBadge: typeof import('element-plus/es')['ElBadge']
